a6338fb99c7b646ca7ba9294ecc1f21d721913cf
Jhoan Buitrago <jhoanuitrago@gmail.com>
2023-11-20 19:13:07 -0500

    Add navbar

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ from utils.db import db
 
 # Models
 
-
 # Routes
-
+from routes.admin import admin
 
 
 def create_app():
@@ -23,10 +22,15 @@ def create_app():
     app.config['SECRET_KEY'] = "4e041161ab1f2548591d829ecdeb58bd3921a59462c714e2ccddbe02b69216d4"
 
     #db.init_app(app)
-
+    app.register_blueprint(admin, url_prefix="/admin")
 
     @app.route('/')
     def index():
-        return render_template("home.html")
+        return render_template("login.html")
     
+    @app.route('/login', methods=['POST', 'GET'])
+    def login():
+        
+        return redirect("/admin")
+
     return app
