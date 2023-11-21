a88e9cbc3771f895f2a2ec6f5ea9d87df4d3bbb7
Jhoan Buitrago <jhoanuitrago@gmail.com>
2023-11-21 16:29:53 -0500

    Fix button style

--- routes/admin.py
+++ routes/admin.py
@@ -14,17 +14,22 @@ def create_user():
         email = request.form['email']
         if len(username) > 30 or not username.isalnum():
             flash('El nombre de usuario debe tener como máximo 30 caracteres alfanuméricos.', 'error')
-            return redirect("/createUser/")
+            return render_template('createUser.html')
         if len(password) > 15 or not password.isalnum():
             flash('La contraseña debe tener como máximo 15 caracteres alfanuméricos.', 'error')
-            return redirect("/createUser/")
+            return render_template('createUser.html')
+        
+        # TODO save to the database
         saveUser(username, password, email)
+
         flash('Usuario creado exitosamente', 'success')
-        return redirect("/admin")
-    elif request.method == 'GET':
         return render_template('createUser.html')
     
+    elif request.method == 'GET':
+        return render_template('createUser.html')
+
+# TODO temp function
 def saveUser(username, password, email):
     filename = 'users.txt'
-    with open(filename, 'a') as file:
-        file.write(f'Username: {username}, Password: {password}, Email: {email}\n')
+    with open(filename, 'a') as f:
+        f.write(f'Username: {username}, Password: {password}, Email: {email}\n')
