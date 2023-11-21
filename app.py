4046c3f0d65e65dc197fdc7705e3827807be4f0e
7ragnaro7 <salarango9@gmail.com>
2023-11-20 22:07:36 -0500

    createUserTask: Functionality

--- app.py
+++ app.py
@@ -33,4 +33,26 @@ def create_app():
         
         return redirect("/admin")
 
+    @app.route('/createUser/', methods=['GET', 'POST'])
+    def create_user():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            email = request.form['email']
+            if len(username) > 30 or not username.isalnum():
+                flash('El nombre de usuario debe tener como máximo 30 caracteres alfanuméricos.', 'error')
+                return redirect("/createUser/")
+            if len(password) > 15 or not password.isalnum():
+                flash('La contraseña debe tener como máximo 15 caracteres alfanuméricos.', 'error')
+                return redirect("/createUser/")
+            saveUser(username, password, email)
+            flash('Usuario creado exitosamente', 'success')
+            return redirect("/admin")
+        return render_template('createUser.html')
+
     return app
+
+def saveUser(username, password, email):
+    filename = 'users.txt'
+    with open(filename, 'a') as file:
+        file.write(f'Username: {username}, Password: {password}, Email: {email}\n')
